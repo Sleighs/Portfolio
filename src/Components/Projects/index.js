0cd5b3c285b65c0dde21ca1bcc8cb4420900d7d6
Samuel Wright <wsright987@gmail.com>
2023-02-28 17:21:58 -0500

    Projects list updated

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -70,10 +70,10 @@ const Projects = (props) => {
             </div>
             <hr/>
 
-            <div className="row" id="project1">
+            <div className="row" id="project1" style={{display:'none'}}>
                 <div className="col-md-7 project-description">
                     <h3 className="projects-row-title"><strong>RealityCalc.com</strong></h3>
-                    <p>Lead developer for RealityCalc.com, home to the popular online tool The Male Reality Calculator hosted at RealityCalc.com. Built with React and Redux, the calculator uses U.S. Census Bureau statistics to calculate the user's odds of meeting their dream woman.</p>
+                    <p>Lead developer for RealityCalc.com, home to the popular online dating tool The Male Reality Calculator hosted at RealityCalc.com. Built with React and Redux, the calculator uses U.S. Census Bureau statistics to calculate the user's odds of meeting their dream woman.</p>
                     <p>
                     <a target="_blank" href="https://realitycalc.com/" rel="noreferrer">Check it out</a>
                     </p>
